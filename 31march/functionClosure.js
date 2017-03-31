1e678c804ce7dcccd843315b31c2c0697ca1127a
appdevworkscripts <nadeem.sajjad@workscripts.in>
2017-03-31 11:40:19 +0530

    new classinjsClass and function closure

---
+++ 31march/functionClosure.js
@@ -0,0 +1,15 @@
+
+var setNewX=(function(){
+    var x=0;
+    return function(){
+        x++;
+        return x;
+    }
+})();
+
+console.log(setNewX());
+console.log(setNewX());
+console.log(setNewX());
+console.log(setNewX());
+console.log(setNewX());
+console.log(setNewX());
