05d9e40558316de29f2732a3b1c30f04f249368b
nady5730642 <nady5730642>
2017-04-06 16:54:43 +0530

    okok

--- 6April/js/app.js
+++ 6April/js/app.js
@@ -12,16 +12,34 @@ app.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
 		templateUrl:'views/contactus.html',
 		controller:'ContactUsController'
 	});
+	$routeProvider.when('/http',{
+		templateUrl:'views/http.html',
+		controller:'HttpController'
+	});
 	$locationProvider.html5Mode(true);
 }]);
 
-app.factory('Mymath',[function(){
+app.factory('Mymath',['$timeout','$q',function($timeout,$q){
 	var obj={
 		square:function(num){
-			return num*num;
+			return $q(function(resolve,reject){
+				$timeout(function(){
+					if(num==0){
+						reject('Eror cannot resolve 0')
+					}
+					resolve(num*num);
+				},2000);
+			});		
 		},
 		cube:function(num){
-			return num*num*num;
+			return $q(function(resolve,reject){
+				$timeout(function(){
+					if(num==0){
+						reject('Eror cannot resolve 0')
+					}
+					resolve(num*num*num);
+				},3000);
+			});	
 		}
 	};
 	return obj;
@@ -45,6 +63,26 @@ app.controller('ContactUsController',['$scope','max1','max2','Mymath','MymathSer
 	}
 	console.log(max1);
 	console.log(max2);
-	console.log(Mymath.square(4));
+	
 	console.log(MymathService.square(5));
+	
+	Mymath.square(4).then(function(result){
+		return Mymath.cube(result);
+	},function(err){
+		console.log('reject'+err);
+	}).then(function(result){
+		console.logFi('nal result = '+result);
+	},function(){
+	});
+}]);
+
+
+app.controller('HttpController',['$scope','$http',function($scope,$http){
+	$scope.submitForm=function(){
+		$http({
+			url:'http://api.fixer.io/latest'
+		}).then(function(response){
+			console.log(response);
+		},function(){});
+	}	
 }]);
